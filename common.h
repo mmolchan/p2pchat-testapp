c3e4d9994df6d2de15217b9555985470efabf6f9
Maxim Molchanov <i.censo@gmail.com>
2020-10-23 20:48:14 -0700

    pchat: initial skeleton

---
+++ common.h
@@ -0,0 +1,12 @@
+/**
+ * @file common.h
+ * @brief Including headers common for the whole app
+ * @author Maxim Molchanov <i.censo[AT]gmail.com>
+ *
+ * 
+ */
+#pragma once
+#include <stdint.h>
+#include <stdbool.h>
+#include <event2/event.h>
+
---
+++ main.c
@@ -0,0 +1,13 @@
+/**
+ * @file main.c
+ * @brief Main event loop
+ * @author Maxim Molchanov <i.censo[AT]gmail.com>
+ *
+ * 
+ */
+
+#include "common.h"
+
+int main(char **argv, int argc) {
+    return 0;
+}
